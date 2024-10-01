8ca62f7909881a0092c72cd7cd9e3742c7c5da89
Mario SHAYA <h2beatz13@gmail.com>
2024-10-01 12:29:30 +0300

    header, fix the active state not working on swap page due to its subdomains

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useEffect } from "react";
 // Next
 import { usePathname } from "next/navigation";
 import Image from "next/image";
@@ -13,16 +14,23 @@ import { useState } from "react";
 // Icons
 import { IoChevronDown, IoHome, IoSwapHorizontal } from "react-icons/io5";
 import { FaCircle } from "react-icons/fa6";
+import clsx from "clsx";
 
 export default function Navbar() {
+	// States
 	const [isMobileMenuActive, setIsMobileMenuActive] = useState(false);
+	const [currentPath, setCurrentPath] = useState<string | null>(null);
 
 	const pathname = usePathname();
 
-	const isLinkActive = (link: string) => {
-		if (link == pathname) {
-			return true;
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			setCurrentPath(window.location.pathname);
 		}
+	}, []);
+
+	const isLinkActive = (link: string) => {
+		return pathname === link || pathname.startsWith(`${link}/`);
 	};
 
 	const currentLinkMobile = () => {
@@ -87,11 +95,12 @@ export default function Navbar() {
 							{navLinksData.map((link, idx) => {
 								return (
 									<Link
-										className={`flex items-center gap-x-2 py-2.5 px-3 ${
+										className={clsx(
+											"flex items-center gap-x-2 py-2.5 px-3",
 											isLinkActive(link.href)
 												? "text-light"
-												: "text-grayText"
-										}`}
+												: "text-grayText",
+										)}
 										href={link.href}
 										target={link.isExternal ? "_blank" : ""}
 										key={idx}
@@ -119,11 +128,12 @@ export default function Navbar() {
 									target={link.isExternal ? "_blank" : ""}
 								>
 									<div
-										className={`transition-all duration-300 ease-in-out ${
+										className={clsx(
+											"transition-all duration-300 ease-in-out",
 											isLinkActive(link.href)
 												? "opacity-1"
-												: "opacity-0"
-										}`}
+												: "opacity-0",
+										)}
 									>
 										{link.icon}
 									</div>
