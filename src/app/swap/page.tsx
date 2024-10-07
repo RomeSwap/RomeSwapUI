f1b19a8de596b4c5fd5e72f66a9ed3429272de4d
Mario SHAYA <h2beatz13@gmail.com>
2024-10-07 16:13:15 +0300

    Fix modal

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -8,15 +8,13 @@ import { Toaster } from "react-hot-toast";
 export default function SwapPage() {
 	return (
 		<Suspense>
-			<div className="h-[calc(100vh-4rem)] w-full flex flex-col items-center justify-center">
-				<div className="absolute z-20">
-					<SwapClient />
-				</div>
+			<div className="h-screen w-full flex flex-col items-center justify-center">
+				<SwapClient />
 				<div className="absolute -left-56 hidden lg:block">
 					<Image
 						src="/colosseum.png"
-						width={1200}
-						height={1200}
+						width={700}
+						height={700}
 						alt="Rome Banner"
 					/>
 				</div>
