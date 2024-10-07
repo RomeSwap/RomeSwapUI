44fe6e04d9b2329fcddea31ec2d257617969d6be
simon <simon@klein-homepage.de>
2024-10-07 13:31:50 +0300

    added /telegram and /twitter redirects.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/twitter',
+        destination: 'https://x.com/rome_swap',
+        permanent: false,
+      },
+      {
+        source: '/telegram',
+        destination: 'https://t.me/+bt04jB2Ckv5jNThi',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
