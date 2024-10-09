331ced7575a1a3127ab9ed15fcfbe326b450f57b
SHAYA Mario <66465021+marioshaya@users.noreply.github.com>
2024-10-09 17:04:52 +0300

    added blurred transparent circles and fixed the bg

--- src/app/swap/layout.tsx
+++ src/app/swap/layout.tsx
@@ -13,12 +13,15 @@ export default function SwapLayout({
   return (
     <main
       className={clsx(
-        "h-screen overflow-hidden bg-[url('/swap-bg.png')] bg-cover bg-center",
+        "h-screen overflow-hidden bg-[url('/swap-bg.png')] bg-cover bg-center bg-fixed",
         "before:absolute before:w-full before:h-full before:top-0 before:right-0 before:bg-background/80 before:z-0",
       )}
     >
-      <div className="absolute left-0 w-full h-full flex flex-col items-center justify-center overflow-hidden">
+      <div className="absolute z-10 left-0 w-full h-full flex flex-col items-center justify-center overflow-hidden">
         {children}
+      </div>
+      <div className="fixed top-0 left-0 w-full h-full overflow-hidden">
+        <div className="hidden lg:block absolute -top-[0px] left-0 w-[300px] h-[300px] bg-[rgba(99,57,249,0.15)] rounded-br-full blur-[150px]"></div>
       </div>
     </main>
   );
