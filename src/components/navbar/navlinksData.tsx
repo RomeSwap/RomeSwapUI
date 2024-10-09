e20443fb3b24dd48daf292f5b91f8046d8c54cc4
simon <simon@klein-homepage.de>
2024-10-09 15:36:23 +0300

    added mercuryo onramp

--- src/components/navbar/navlinksData.tsx
+++ src/components/navbar/navlinksData.tsx
@@ -17,8 +17,8 @@ export const navLinksData = [
     isExternal: true,
   },
   {
-    name: "Info",
-    href: "https://www.icecreamswap.com/info",
+    name: "Onramp",
+    href: "https://exchange.mercuryo.io/?widget_id=d950fbc7-9c58-4f04-bedc-dad3dd03d004&type=buy&network=SOLANA&currency=SOL&signature=8b1ebb7fbe0f56280f42eaf8e2cb7ebabd4f3c3d760373c214c509b3d7ffe50a",
     isExternal: true,
   },
 ];
